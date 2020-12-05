64fa08d75e342a9f4e6d9ae0cefd9b1132a7abb4
Oleg_Kozhemiakin <o.kozhemiakin@gmail.com>
2020-12-05 20:27:57 +0200

    handle deposit and withdraw events

--- src/dodoPair.ts
+++ src/dodoPair.ts
@@ -1,5 +1,5 @@
-import { SellBaseToken, BuyBaseToken} from "../generated/templates/DODOPairTemplate/DODOPair";
-import {DODOPair, Token, Trade} from "../generated/schema";
+import { SellBaseToken, BuyBaseToken, Deposit as DepositEvent, Withdraw as WithdrawEvent} from "../generated/templates/DODOPairTemplate/DODOPair";
+import {DODOPair, Token, Trade, Deposit, Withdraw} from "../generated/schema";
 import {convertTokenToDecimal, ZERO_BIG_DECIMAL} from "./helpers";
 import {BigDecimal} from "@graphprotocol/graph-ts/index";
 
@@ -32,4 +32,38 @@ export function handleBaseBuy(event: BuyBaseToken) : void{
     trade.quoteSell = quoteSellAmount
     trade.quoteBuy = ZERO_BIG_DECIMAL
     trade.save()
+}
+
+export function handleDeposit(event: DepositEvent){
+    let dodoPair = DODOPair.load(event.address.toHexString())
+    let depositedToken
+    if(event.params.isBaseToken) {
+        depositedToken = Token.load(dodoPair.baseToken)
+    } else {
+        depositedToken = Token.load(dodoPair.quoteToken)
+    }
+    let deposit = new Deposit(event.transaction.hash.toHexString())
+    deposit.dodoPair = dodoPair.id
+    deposit.deposited = depositedToken
+    deposit.payer = event.params.payer
+    deposit.receiver = event.params.receiver
+    deposit.amount = convertTokenToDecimal(event.params.amount, depositedToken.decimals)
+    deposit.lpTokenAmount = convertTokenToDecimal(event.params.lpTokenAmount, depositedToken.decimals)
+}
+
+export function handleWithdraw(event: WithdrawEvent){
+    let dodoPair = DODOPair.load(event.address.toHexString())
+    let withdrawedToken
+    if(event.params.isBaseToken) {
+        withdrawedToken = Token.load(dodoPair.baseToken)
+    } else {
+        withdrawedToken = Token.load(dodoPair.quoteToken)
+    }
+    let withdraw = new Withdraw(event.transaction.hash.toHexString())
+    withdraw.dodoPair = dodoPair.id
+    withdraw.deposited = withdrawedToken
+    withdraw.payer = event.params.payer
+    withdraw.receiver = event.params.receiver
+    withdraw.amount = convertTokenToDecimal(event.params.amount, withdrawedToken.decimals)
+    withdraw.lpTokenAmount = convertTokenToDecimal(event.params.lpTokenAmount, withdrawedToken.decimals)
 }
